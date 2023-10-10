ff15283e84220f92c5c9f7e00102a2ad7963e481
meganmcadams <megmcmuffin14@gmail.com>
2023-10-09 22:00:16 -0500

    specifying static folder

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ from _utils.officers import parse_data
 import sys
 
 # Initialize Flask app
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder="./static")
 
 f = open('officers.txt')
 data = f.read()
