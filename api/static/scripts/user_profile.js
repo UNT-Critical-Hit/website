ae8c2479da7d90c85f2c83c0f12bf0c92acee892
meganmcadams <megmcmuffin14@gmail.com>
2024-02-07 21:05:34 -0600

    updated officers

--- api/static/scripts/user_profile.js
+++ api/static/scripts/user_profile.js
@@ -1,17 +1,21 @@
-let unt_student = document.getElementById('user_unt_student').value;
-if (unt_student == "True") {
-    document.getElementById('unt_student_yes').setAttribute('selected', true);
-} else if(unt_student == "False") {
-    document.getElementById('unt_student_no').setAttribute('selected', false);
-    toggle_email(false);
-}
+window.addEventListener('load', function() {
 
-let alert_card = document.getElementById('alert_card');
+    let unt_student = document.getElementById('user_unt_student').value;
+    if (unt_student == "True") {
+        document.getElementById('unt_student_yes').setAttribute('selected', true);
+    } else if(unt_student == "False") {
+        document.getElementById('unt_student_no').setAttribute('selected', false);
+        toggle_email(false);
+    }
 
-let updated = document.getElementById('updated');
-if (document.getElementById('alert_card_message').innerHTML) {
-    alert_card.style.display = "";
-}
-document.getElementById('close_card').addEventListener('click', function() {
-    alert_card.remove();
+    let alert_card = document.getElementById('alert_card');
+
+    let updated = document.getElementById('updated');
+    if (document.getElementById('alert_card_message').innerHTML) {
+        alert_card.style.display = "";
+    }
+    document.getElementById('close_card').addEventListener('click', function() {
+        alert_card.remove();
+    });
+
 });
